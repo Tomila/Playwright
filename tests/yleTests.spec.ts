94eaa3589f8632e4f5724b0dc8244415e3582c67
Roman Prokhozhev <roman.prokhozhev@metropolia.fi>
2022-11-08 21:45:19 +0200

    New tests added

---
+++ tests/yleTests.spec.ts
@@ -0,0 +1,52 @@
+import { test, expect } from '@playwright/test';
+
+test('"Luo YLE tunnus"- page visible', async ({ page,browserName }) => {
+    await page.goto('https://areena.yle.fi/tv');
+    if (browserName != 'chromium') {
+        await page.getByRole('button', { name: 'Vain välttämättömät' }).click();
+    }
+    await page.getByRole('button', { name: 'Kirjaudu' }).click();
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByRole('link', { name: 'Luo Yle Tunnus' }).click();
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByRole('heading', { name: 'Luo Yle Tunnus' }).click();
+});
+
+test('page doesn`t accept emails in wrong format', async ({ page, browserName }) => {
+    await page.goto('https://areena.yle.fi/tv');
+    if (browserName != 'chromium') {
+        await page.getByRole('button', { name: 'Vain välttämättömät' }).click();
+    }
+    await page.getByRole('button', { name: 'Kirjaudu' }).click();
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByRole('link', { name: 'Luo Yle Tunnus' }).click();
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByLabel('Sähköposti').click();
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByLabel('Sähköposti').fill('wrongformat.mail.com');
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByLabel('Salasana').click();
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByText('Tarkista sähköpostiosoitteen muoto.').click();
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByLabel('Sähköposti').click();
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByText('Luo Yle TunnusOnko sinulla jo Yle Tunnus? Kirjaudu sisäänKirjautuneena saat henk').click();
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByLabel('Sähköposti').fill('123mail.com');
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByText('Tarkista sähköpostiosoitteen muoto.').click();
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByLabel('Sähköposti').click();
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByLabel('Sähköposti').fill('@mail.com.com');
+    await page.frameLocator('role=dialog[name="kirjaudu sisään"] >> iframe').getByText('Tarkista sähköpostiosoitteen muoto.').click();
+});
+
+test('"Kummeli" - series page visible', async ({ page , browserName}) => {
+    await page.goto('https://areena.yle.fi/1-3339547');
+    if (browserName != 'chromium') {
+        await page.getByRole('button', { name: 'Vain välttämättömät' }).click();
+    }
+    await page.locator('section:has-text("KummeliKatso: K1, J17 kauttaTaattua Kummelihuumoria. Kummeli tavoittelee sitä su")').click();
+});
+
+test('"Kummeli" - series 3rd season, 5th episodes name and date can be found', async ({ page, browserName }) => {
+    await page.goto('https://areena.yle.fi/1-3339547');
+    if (browserName != 'chromium') {
+        await page.getByRole('button', { name: 'Vain välttämättömät' }).click();
+    }
+    await page.locator('section:has-text("KummeliKatso: K1, J17 kauttaTaattua Kummelihuumoria. Kummeli tavoittelee sitä su")').click();
+    await page.getByRole('button', { name: 'Kausi 3' }).click();
+    await page.getByRole('link', { name: 'Jakso 5: Kummeli' }).click();
+    await expect(page).toHaveURL('https://areena.yle.fi/1-1796319');
+    await page.getByRole('heading', { name: 'K3, J5: Kummeli' }).click();
+    await page.getByText('julkaistu ti 10.1.2006').click();
+});
